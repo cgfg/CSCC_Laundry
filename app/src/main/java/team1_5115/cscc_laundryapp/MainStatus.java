bce56e1c7ea3f2ab9e602a9c37d3f8a31a83f5de
orour138 <orour138@umn.edu>
2016-03-30 20:15:14 -0500

    Initial commit

---
+++ app/src/main/java/team1_5115/cscc_laundryapp/MainStatus.java
@@ -0,0 +1,13 @@
+package team1_5115.cscc_laundryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_status);
+    }
+}
