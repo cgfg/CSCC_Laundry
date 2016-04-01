130ba37a6ae9df73a79d0714644ccc562b2c4ee5
cgfg <1991chenxi@gmail.com>
2016-04-01 18:07:40 -0500

    Add Maintenance Activity UI

--- app/src/main/java/team1_5115/cscc_laundryapp/Maintenance.java
+++ app/src/main/java/team1_5115/cscc_laundryapp/Maintenance.java
@@ -1,7 +1,10 @@
 package team1_5115.cscc_laundryapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class Maintenance extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class Maintenance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maintenance);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.maintenance_radioButton1:
+                if (checked) {
+                    Toast.makeText(this, "You selected Problem 1", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+            case R.id.maintenance_radioButton2:
+                if (checked) {
+                    Toast.makeText(this.getBaseContext(), "You selected Problem 2", Toast.LENGTH_LONG).show();
+                    break;
+                }
+            case R.id.maintenance_radioButton3:
+                if (checked) {
+                    Toast.makeText(this.getBaseContext(),"You selected Problem 3", Toast.LENGTH_LONG).show();
+                    break;
+                }
+            case R.id.maintenance_radioButton4:
+                if (checked) {
+                    Toast.makeText(this.getBaseContext(),"You selected Other", Toast.LENGTH_LONG).show();
+                    break;
+                }
+        }
     }
 }
