5577f17cc631de98827678b28f5abde4c9a3d783
Xi Chen <1991chenxi@gmail.com>
2016-04-01 22:03:21 -0500

    add menu

--- app/src/main/java/team1_5115/cscc_laundryapp/MainStatus.java
+++ app/src/main/java/team1_5115/cscc_laundryapp/MainStatus.java
@@ -3,6 +3,9 @@ package team1_5115.cscc_laundryapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainStatus extends AppCompatActivity {
@@ -17,5 +20,29 @@ public class MainStatus extends AppCompatActivity {
         Intent intent = new Intent(MainStatus.this,PopupMachineSelect.class);
         startActivity(intent);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.settings:
+                gotoSettingsScreen();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void gotoSettingsScreen(){
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }
