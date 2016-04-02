dc633f55207049969061d841d04b952a81b7eb0a
orour138 <orour138@umn.edu>
2016-04-01 20:14:15 -0500

    Work on Main Status Screen: set up machine layout, configured popup window for machines.

--- app/src/main/java/team1_5115/cscc_laundryapp/MainStatus.java
+++ app/src/main/java/team1_5115/cscc_laundryapp/MainStatus.java
@@ -1,7 +1,9 @@
 package team1_5115.cscc_laundryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainStatus extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainStatus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_status);
+    }
+
+    public void machineClickHandler(View view) {
+        Intent intent = new Intent(MainStatus.this,PopupMachineSelect.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/team1_5115/cscc_laundryapp/PopupMachineSelect.java
@@ -0,0 +1,40 @@
+package team1_5115.cscc_laundryapp;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+/**
+ * Created by Adam on 4/1/2016.
+ */
+public class PopupMachineSelect extends MainStatus{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popup_machine_options);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width * .75),(int) (height * .35));
+    }
+
+    /* called when user presses 'Use Machine' button from popup window
+    update machine status so proper machine is updated to in use by user
+    redirect to activity_main_status
+    */
+    public void clickUseMachine(View view) {
+
+    }
+
+    /* called when user presses 'Maintenance Request' button
+    redirect user to maintenance screen
+     */
+    public void clickMaintenanceRequest(View view) {
+
+    }
+}
