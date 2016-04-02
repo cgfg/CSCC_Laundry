eaa04be8551236b00366d89763c3e21d51655372
Xi Chen <1991chenxi@gmail.com>
2016-04-01 23:23:49 -0500

    update settings screen

--- app/src/main/java/team1_5115/cscc_laundryapp/Settings.java
+++ app/src/main/java/team1_5115/cscc_laundryapp/Settings.java
@@ -2,6 +2,8 @@ package team1_5115.cscc_laundryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +11,39 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void clickEdit(View view){
+        EditText editText = (EditText)findViewById(R.id.firstNameEdit);
+        editText.setFocusableInTouchMode(true);
+        editText = (EditText)findViewById(R.id.lastNameEdit);
+        editText.setFocusableInTouchMode(true);
+        editText = (EditText)findViewById(R.id.emailEdit);
+        editText.setFocusableInTouchMode(true);
+        editText = (EditText)findViewById(R.id.phoneNumEdit);
+        editText.setFocusableInTouchMode(true);
+        editText = (EditText)findViewById(R.id.buildingNumEdit);
+        editText.setFocusableInTouchMode(true);
+        editText = (EditText)findViewById(R.id.apartNumEdit);
+        editText.setFocusableInTouchMode(true);
+        editText = (EditText)findViewById(R.id.passwordEdit);
+        editText.setFocusableInTouchMode(true);
+    }
+
+    public void clickSubmit(View view){
+        EditText editText = (EditText)findViewById(R.id.firstNameEdit);
+        editText.setFocusable(false);
+        editText = (EditText)findViewById(R.id.lastNameEdit);
+        editText.setFocusable(false);
+        editText = (EditText)findViewById(R.id.emailEdit);
+        editText.setFocusable(false);
+        editText = (EditText)findViewById(R.id.phoneNumEdit);
+        editText.setFocusable(false);
+        editText = (EditText)findViewById(R.id.buildingNumEdit);
+        editText.setFocusable(false);
+        editText = (EditText)findViewById(R.id.apartNumEdit);
+        editText.setFocusable(false);
+        editText = (EditText)findViewById(R.id.passwordEdit);
+        editText.setFocusable(false);
     }
 }
