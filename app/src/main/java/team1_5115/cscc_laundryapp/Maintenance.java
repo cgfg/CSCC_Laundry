25741320f8b63c0e4b8894787a92141d57e638db
saseeri <sahar.aseeri@kaust.edu.sa>
2016-04-01 21:22:01 -0500

    Created settings page

--- app/src/main/java/team1_5115/cscc_laundryapp/Maintenance.java
+++ app/src/main/java/team1_5115/cscc_laundryapp/Maintenance.java
@@ -1,7 +1,14 @@
 package team1_5115.cscc_laundryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Maintenance extends AppCompatActivity {
 
@@ -9,5 +16,58 @@ public class Maintenance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maintenance);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.maintenance_radioButton1:
+                if (checked) {
+                    Toast.makeText(this, "You selected Problem 1", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+            case R.id.maintenance_radioButton2:
+                if (checked) {
+                    Toast.makeText(this.getBaseContext(), "You selected Problem 2", Toast.LENGTH_LONG).show();
+                    break;
+                }
+            case R.id.maintenance_radioButton3:
+                if (checked) {
+                    Toast.makeText(this.getBaseContext(),"You selected Problem 3", Toast.LENGTH_LONG).show();
+                    break;
+                }
+            case R.id.maintenance_radioButton4:
+                if (checked) {
+                    Toast.makeText(this.getBaseContext(),"You selected Other", Toast.LENGTH_LONG).show();
+                    break;
+                }
+        }
+    }
+
+    public void onSubmitButtonClicked(View view) {
+        //check if there is radio button selected
+        RadioGroup rg = (RadioGroup) findViewById(R.id.maintenance_radioGroup);
+        String message;
+        if (rg.getCheckedRadioButtonId() < 0) {
+            message = "Please select one problem";
+        } else {
+            message = "A maintenance request has been sent.";
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+        EditText description = (EditText) findViewById(R.id.maintenance_editText);
+        description.setText("");
+        rg.clearCheck();
     }
 }
