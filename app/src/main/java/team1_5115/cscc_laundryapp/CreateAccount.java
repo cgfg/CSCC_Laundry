0b9cb18633b937245e17f9168a5f572a8bc86ac7
saseeri <sahar.aseeri@kaust.edu.sa>
2016-04-04 17:39:29 -0500

    Added Ash's work

--- app/src/main/java/team1_5115/cscc_laundryapp/CreateAccount.java
+++ app/src/main/java/team1_5115/cscc_laundryapp/CreateAccount.java
@@ -2,6 +2,8 @@ package team1_5115.cscc_laundryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+    }
+
+    public void onButtonClick(View v){
+
+        if(v.getId() == R.id.button2 ){
+
+            Intent i = new Intent(this, Login.class);
+            startActivity(i);
+        }
     }
 }
