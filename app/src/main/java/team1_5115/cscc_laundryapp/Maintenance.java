9c27d9c035925ef70ec92402404d65a46a9a7dee
Yangyang Tang <lizzy220@qq.com>
2016-04-26 23:07:39 -0500

    add confirm maintenance request screen

--- app/src/main/java/team1_5115/cscc_laundryapp/Maintenance.java
+++ app/src/main/java/team1_5115/cscc_laundryapp/Maintenance.java
@@ -1,26 +1,25 @@
 package team1_5115.cscc_laundryapp;
 
-import android.app.AlertDialog;
+import android.app.FragmentManager;
 import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
-public class Maintenance extends AppCompatActivity {
+public class Maintenance extends AppCompatActivity implements MaintenanceIssueFragment.OnFragmentInteractionListener, MaintenanceConfirmFragment.OnFragmentInteractionListener {
     private String machineId = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maintenance);
-//        Bundle extras = getIntent().getExtras();
-//        if (extras.containsKey("MachineId")) {
-//            machineId = extras.getString("MachineId");
-//        }
-//        TextView selectText = (TextView) findViewById(R.id.maintenance_select_text);
-//        selectText.setText("Select one problem for " + machineId);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        MaintenanceIssueFragment issueFragment = new MaintenanceIssueFragment();
+        transaction.add(R.id.maintenance_container, issueFragment, "issue_fragment");
+        transaction.commit();
+
     }
 
     public void onRadioButtonClicked(View view) {
@@ -47,31 +46,29 @@ public class Maintenance extends AppCompatActivity {
 //        }
     }
 
-    public void onSubmitButtonClicked(View view) {
-//        //check if there is radio button selected
-//        RadioGroup rg = (RadioGroup) findViewById(R.id.maintenance_radioGroup);
-//        String message;
-//        final int isChecked = rg.getCheckedRadioButtonId();
-//        if ( isChecked < 0) {
-//            message = "Please select one problem for " + machineId;
-//        } else {
-//            message = "A maintenance request for " + machineId + " has been sent.";
-//        }
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//        builder.setMessage(message)
-//                .setCancelable(false)
-//                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        dialog.dismiss();
-//                        if (isChecked >= 0)
-//                            finish();
-//                    }
-//                });
-//        AlertDialog alert = builder.create();
-//        alert.show();
+    public void onIssueSubmitButtonClicked(View view) {
+        MaintenanceIssueFragment oldFragment = (MaintenanceIssueFragment) getSupportFragmentManager().findFragmentById(R.id.maintenance_issue_fragment);
+        MaintenanceConfirmFragment newFragment = new MaintenanceConfirmFragment();
+        Bundle args = new Bundle();
+        newFragment.setArguments(args);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.maintenance_container, newFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void onConfirmSubmitButtonClicked(View view) {
+
     }
 
     public void onCancelButtonClicked(View view) {
         finish();
     }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
 }
