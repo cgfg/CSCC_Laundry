c2150e410448df5ddd93916f96a86f124e0d096b
Yangyang Tang <lizzy220@qq.com>
2016-04-27 13:17:20 -0500

    fix incorrect text style in maintenance issue

--- app/src/main/java/team1_5115/cscc_laundryapp/Maintenance.java
+++ app/src/main/java/team1_5115/cscc_laundryapp/Maintenance.java
@@ -2,12 +2,15 @@ package team1_5115.cscc_laundryapp;
 
 import android.app.FragmentManager;
 import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class Maintenance extends AppCompatActivity implements MaintenanceIssueFragment.OnFragmentInteractionListener, MaintenanceConfirmFragment.OnFragmentInteractionListener {
@@ -24,27 +27,15 @@ public class Maintenance extends AppCompatActivity implements MaintenanceIssueFr
     }
 
     public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-//        boolean checked = ((RadioButton) view).isChecked();
-//
-//        // Check which radio button was clicked
-//        switch(view.getId()) {
-//            case R.id.maintenance_radioButton1:
-//                if (checked) {
-////                    Toast.makeText(this, "You selected Problem 1", Toast.LENGTH_SHORT).show();
-//                    break;
-//                }
-//            case R.id.maintenance_radioButton2:
-//                if (checked) {
-////                    Toast.makeText(this.getBaseContext(), "You selected Problem 2", Toast.LENGTH_LONG).show();
-//                    break;
-//                }
-//            case R.id.maintenance_radioButton3:
-//                if (checked) {
-////                    Toast.makeText(this.getBaseContext(),"You selected Other", Toast.LENGTH_LONG).show();
-//                    break;
-//                }
-//        }
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.maintenance_radio_group);
+        RadioButton radio_1 = (RadioButton) findViewById(R.id.issues_1_radio);
+        RadioButton radio_2 = (RadioButton) findViewById(R.id.issues_2_radio);
+        RadioButton radio_3 = (RadioButton) findViewById(R.id.issues_3_radio);
+        radio_1.setTypeface(null, Typeface.NORMAL);
+        radio_2.setTypeface(null, Typeface.NORMAL);
+        radio_3.setTypeface(null, Typeface.NORMAL);
+        RadioButton newRadio = (RadioButton) radioGroup.findViewById(view.getId());
+        newRadio.setTypeface(null, Typeface.BOLD);
     }
 
     public void onIssueSubmitButtonClicked(View view) {
