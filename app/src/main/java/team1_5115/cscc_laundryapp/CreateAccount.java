5fb6b7a36f58ba4b69f2793f935e1a6f262e89a3
saseeri <sahar.aseeri@kaust.edu.sa>
2016-05-01 17:27:34 -0500

    Update on login, singup, and create account with the main page

--- app/src/main/java/team1_5115/cscc_laundryapp/CreateAccount.java
+++ app/src/main/java/team1_5115/cscc_laundryapp/CreateAccount.java
@@ -4,20 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
 
-public class CreateAccount extends AppCompatActivity {
+public class CreateAccount extends AppCompatActivity implements View.OnClickListener {
+
+    Button creat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        creat = (Button) findViewById(R.id.button3);
+        creat.setOnClickListener(this);
     }
 
-    public void onButtonClick(View v){
-
-        if(v.getId() == R.id.button2 ){
-            Intent i = new Intent(this, Login.class);
-            startActivity(i);
-        }
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(CreateAccount.this,MainStatus.class);
+        intent.putExtra("id", v.getId());
+        startActivity(intent);
     }
 }
--- app/src/main/java/team1_5115/cscc_laundryapp/Login.java
+++ app/src/main/java/team1_5115/cscc_laundryapp/Login.java
@@ -1,13 +1,45 @@
 package team1_5115.cscc_laundryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    Button logIn;
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        logIn = (Button) findViewById(R.id.button);
+        logIn.setOnClickListener(this);
+
+        signUp = (Button) findViewById(R.id.button2);
+        signUp.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.button ){
+            Intent intent = new Intent(Login.this,MainStatus.class);
+            intent.putExtra("id", v.getId());
+            startActivity(intent);
+        }
+        else
+        {
+            Intent intent = new Intent(Login.this,CreateAccount.class);
+            intent.putExtra("id", v.getId());
+            startActivity(intent);
+
+        }
+
+    }
+
 }
